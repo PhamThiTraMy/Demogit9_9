7ae4b4c11b478d885e8d8e44bb74da62eac943c1
PhamThiTraMy <tramy06spt@gmail.com>
2025-09-09 07:58:00 +0700

    init project

---
+++ code_demo_9_9/src/main/java/com/mycompany/code_demo_9_9/Code_demo_9_9.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.code_demo_9_9;
+
+/**
+ *
+ * @author TraMy
+ */
+public class Code_demo_9_9 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Tra My!");
+    }
+}
